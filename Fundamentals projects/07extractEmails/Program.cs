c83e1951744af3de718152e342ba31e18bf443fc
d77commerce <73217857+d77commerce@users.noreply.github.com>
2021-08-11 16:48:06 +0100

    new projects

---
+++ Fundamentals projects/07extractEmails/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _07extractEmails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string pattern = @"\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*";
+            MatchCollection match = Regex.Matches(input, pattern);
+            foreach (var item in match)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
