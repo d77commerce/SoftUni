9c3175ae525a73cab57159023efcb46418a28ee5
d77commerce <73217857+d77commerce@users.noreply.github.com>
2021-08-11 17:07:14 +0100

    new project

---
+++ Fundamentals projects/04caesarCipher/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _04caesarCipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            StringBuilder sb = new StringBuilder(input);
+           
+            for (int i = 0; i < sb.Length; i++)
+            {
+                int charInt = (int)sb[i]+3;
+                char newChar = Convert.ToChar(charInt);
+
+                sb[i] = newChar;
+            }
+            Console.WriteLine(sb);
+        }
+    }
+}
