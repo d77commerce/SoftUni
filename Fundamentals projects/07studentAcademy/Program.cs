e1032ead3ba604bafa3e439957659d67e3dfd4ae
d77commerce <73217857+d77commerce@users.noreply.github.com>
2021-08-13 17:32:54 +0100

    new

---
+++ Fundamentals projects/07studentAcademy/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07studentAcademy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string name = string.Empty;
+            decimal grade = 0;
+            var result = new Dictionary<string, List<decimal>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                name = Console.ReadLine();
+                grade = decimal.Parse(Console.ReadLine());
+                if (!result.ContainsKey(name))
+                {
+                    result.Add(name, new List<decimal>());
+                }
+                result[name].Add(grade);
+            }
+            foreach (var student in result.OrderByDescending(x => x.Value.Average())) 
+            {
+                decimal totalDegre = 0;
+                foreach (var degre in student.Value)
+                {
+                    totalDegre += degre;
+                }
+                totalDegre = totalDegre / student.Value.Count;
+                if (totalDegre >= (decimal)4.5)
+                {
+
+                    Console.WriteLine($"{student.Key} -> {totalDegre:F2}");
+                }
+
+            }
+        }
+    }
+}
---
+++ Fundamentals projects/phone/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace phone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> inPut = Console.ReadLine()
+                .Split(", ")
+                .ToList();
+            string command = Console.ReadLine();
+            while (command != "End")
+            {
+                string[] commandArray = command.Split(" - ");
+                string action = commandArray[0];
+                string phone = commandArray[1];
+                if (action == "Add")
+                {
+                    if (!inPut.Contains(phone))
+                    {
+                        inPut.Add(phone);
+                    }
+
+                }
+                if (action == "Remove")
+                {
+                    if (inPut.Contains(phone))
+                    {
+                        inPut.Remove(phone);
+                    }
+                }
+                if (action == "Bonus phone")
+                {
+                    string[] bonusPhone = commandArray[1].Split(":");
+                    string oldPhone = bonusPhone[0];
+                    string newPhone = bonusPhone[1];
+                    if (inPut.Contains(oldPhone))
+                    {
+                        int indexOldPhone = inPut.IndexOf(oldPhone);
+                        inPut.Insert(indexOldPhone + 1, newPhone);
+                    }
+
+                }
+                if (action == "Last")
+                {
+                    if (inPut.Contains(phone))
+                    {
+                        inPut.Remove(phone);
+                        inPut.Add(phone);
+                    }
+
+                }
+                command = Console.ReadLine();
+            }
+            Console.WriteLine(string.Join(", ",inPut));
+        }
+    }
+}
