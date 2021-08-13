cc7c56a7fc5ffd251c9b08b1c5f52c3eb9d8a0ff
d77commerce <73217857+d77commerce@users.noreply.github.com>
2021-08-13 23:11:11 +0100

    new

---
+++ Fundamentals projects/time+15/time+15/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace time_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hourse = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+
+
+        }
+    }
+}
