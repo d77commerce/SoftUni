986bc4819950c0544c9be406005e6bf969bcf837
d77commerce <73217857+d77commerce@users.noreply.github.com>
2021-08-14 15:53:24 +0100

    Exam SoftUni Fundamentals

---
+++ Fundamentals projects/Exam - 14.08.2021/03exam/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03exam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string names = string.Empty;
+            Dictionary<string, List<string>> nameMeal = new Dictionary<string, List<string>>();
+            int unLikeMeal = 0;
+            while (input != "Stop")
+            {
+                string[] inputArray = input.Split("-");
+                if (inputArray[0] == "Like")
+                {
+                    if (!nameMeal.ContainsKey(inputArray[1]))
+                    {
+                        nameMeal.Add(inputArray[1], new List<string>() { inputArray[2] });
+                    }
+                    else
+                    {
+                        if (!nameMeal[inputArray[1]].Contains(inputArray[2]))
+                        {
+                            nameMeal[inputArray[1]].Add(inputArray[2]);
+                        }
+                    }
+
+                }
+                else
+                {
+                    if (!nameMeal.ContainsKey(inputArray[1]))
+                    {
+                        Console.WriteLine($"{inputArray[1]} is not at the party.");
+                        input = Console.ReadLine();
+                        continue;
+                    }
+                    if (nameMeal[inputArray[1]].Contains(inputArray[2]))
+                    {
+                        nameMeal[inputArray[1]].Remove(inputArray[2]);
+                        Console.WriteLine($"{inputArray[1]} doesn't like the {inputArray[2]}.");
+                        input = Console.ReadLine();
+                        continue;
+                    }
+                    if (!nameMeal[inputArray[1]].Contains(inputArray[2]))
+                    {
+                        nameMeal[inputArray[1]].Remove(inputArray[2]);
+                        Console.WriteLine($"{ inputArray[1]} doesn't have the {inputArray[2]} in his/her collection.");
+                        input = Console.ReadLine();
+                        continue;
+                    }
+
+
+                }
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var item in nameMeal)
+            {
+                Console.WriteLine(item.Key);
+                Console.WriteLine(item.Value);
+            }
+        }
+    }
+}
---
+++ Fundamentals projects/Exam - 14.08.2021/exam/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace exam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string patName = @"\|([A-Z]{2,})\|";
+            string pat = @"\|([A-Z]{2,})\|\:#([A-z]{1,} [A-z]{1,})#";
+            string patTitle = @"\|\:#([A-z]{1,} [A-z]{1,})#";
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                Regex regex = new Regex(pat);
+                MatchCollection matches = regex.Matches(input);
+                if (!regex.IsMatch(input))
+                {
+                    Console.WriteLine($"Access denied!");
+                }
+                foreach (Match item in matches)
+                {
+                    Console.WriteLine($"{item.Groups[1]}, The {item.Groups[2]}");
+                    Console.WriteLine($">> Strength: {item.Groups[1].Length}");
+                    Console.WriteLine($">> Armor: {item.Groups[2].Length}");
+                }
+
+            }
+        }
+    }
+}
