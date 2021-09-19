fbb5857628e50dfdd402b91a7c5393841af8b284
Danail Tsachev <73217857+d77commerce@users.noreply.github.com>
2021-09-19 23:30:28 +0300

    new repo

---
+++ C# Advanced/stacks/ConsoleApp1/03simpleCalculator/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03simpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inPut = Console.ReadLine().Split();
+            Stack<string> num = new Stack<string>(inPut.Reverse());
+            int total = 0;
+            while (num.Count > 0)
+            {
+                if (num.Peek() == "+")
+                {
+                    num.Pop();
+                    total += int.Parse(num.Pop());
+                }
+                else if (num.Peek() == "-")
+                {
+                    num.Pop();
+                    total -= int.Parse(num.Pop());
+                }
+                else
+                {
+                    total += int.Parse(num.Pop());
+                }
+            }
+            Console.WriteLine(total);
+        }
+    }
+}
