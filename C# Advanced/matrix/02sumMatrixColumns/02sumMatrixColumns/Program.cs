eca906f3340b784e3a6232392b249d8b2ceaeaa1
d77commerce <73217857+d77commerce@users.noreply.github.com>
2021-09-20 22:06:08 +0100

    matrix

---
+++ C# Advanced/matrix/02sumMatrixColumns/02sumMatrixColumns/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace _02sumMatrixColumns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] matrixSize = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            int rows = matrixSize[0];
+            int cols = matrixSize[1];
+            int[,] matrix = new int[rows, cols];
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = input[col];
+                }
+            }
+            int ColSum = 0;
+            for (int i = 0; i < matrix.GetLength(1); i++)
+            {
+                for (int j = 0; j <matrix.GetLength(0) ; j++)
+                {
+                    ColSum += matrix[j, i];
+                }
+                Console.WriteLine(ColSum);
+                ColSum = 0;
+            }
+        }
+    }
+}
