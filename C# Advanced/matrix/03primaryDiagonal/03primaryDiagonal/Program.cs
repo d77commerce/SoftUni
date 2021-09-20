8001b8a4b2e52b92efc73f675c1e034934c1ed69
d77commerce <73217857+d77commerce@users.noreply.github.com>
2021-09-20 22:45:25 +0100

    new repo

---
+++ C# Advanced/matrix/03primaryDiagonal/03primaryDiagonal/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace _03primaryDiagonal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int matrixSize = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[matrixSize, matrixSize];
+            int sum = 0;
+            for (int row = 0; row < matrixSize; row++)
+            {
+                int[] input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+                for (int col = 0; col < matrixSize; col++)
+                {
+                    matrix[row, col] = input[col];
+                }
+            }
+            int count = 0;
+            while(count<matrixSize)
+            {
+                sum += matrix[count, count];
+                count++;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
