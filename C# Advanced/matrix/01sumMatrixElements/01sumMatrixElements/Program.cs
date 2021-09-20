aac50ca817e41050ef3512fdede0aa03c8c2e464
d77commerce <73217857+d77commerce@users.noreply.github.com>
2021-09-20 21:21:22 +0100

    new

---
+++ C# Advanced/matrix/01sumMatrixElements/01sumMatrixElements/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace _01sumMatrixElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] matrixSize = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            int rows = matrixSize[0];
+            int cols = matrixSize[1];
+            int[,] matrix = new int[rows, cols];
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] input = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = input[col];
+                }
+            }
+            int total = 0;
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    total += matrix[i, j];
+                }
+            }
+            Console.WriteLine(matrix.GetLength(0));
+            Console.WriteLine(matrix.GetLength(1));
+            Console.WriteLine(total);
+        }
+    }
+}
