16694d82b17efdaf2d1a26055a27e5acb314248b
d77commerce <73217857+d77commerce@users.noreply.github.com>
2021-09-22 22:46:17 +0100

    multiDictonaris

---
+++ C# Advanced/Multi-Dictionaries/1countSameValuesArray/1countSameValuesArray/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _1countSameValuesArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double [] input = Console.ReadLine().Split(" ").Select(double.Parse).ToArray();
+            var countFunction = new Dictionary<double, int>();
+            foreach (var i in input)
+     
+            {
+                if (!countFunction.ContainsKey(i))
+                {
+                    countFunction[i] = 0;
+                }
+                countFunction[i] ++;
+            }
+            foreach (var item in countFunction)
+            {
+                Console.WriteLine($"{item.Key} - {item.Value} times");
+            }
+        }
+    }
+}
---
+++ C# Advanced/Multi-Dictionaries/1countSameValuesArray/2. Average Student Grades/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _2._Average_Student_Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            var studentsArray = new Dictionary<string, List<decimal>>();
+            for (int i = 0; i < num; i++)
+            {
+                string[] newStudent = Console.ReadLine().Split(' ');
+                string student = newStudent[0];
+                decimal grades = decimal.Parse(newStudent[1]);
+                if (!studentsArray.ContainsKey(student))
+                {
+                    studentsArray[student] = new List<decimal>();
+                }
+                studentsArray[student].Add(grades);
+
+            }
+            foreach (var student in studentsArray)
+            {
+                Console.Write(student.Key + " -> ");
+                decimal averDegre =0;
+                foreach (var item in student.Value)
+                {
+                    Console.Write($"{item:f2} ");
+                   
+                    averDegre += item;
+                } 
+                Console.Write($"(avg: {averDegre/student.Value.Count:F2})");
+                Console.WriteLine();
+            }
+        }
+    }
+}
