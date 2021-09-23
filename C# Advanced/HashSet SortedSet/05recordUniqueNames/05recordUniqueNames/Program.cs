7ec16d01b8be08fefe3f25a6d11d805315e1a4ad
d77commerce <73217857+d77commerce@users.noreply.github.com>
2021-09-23 22:06:47 +0100

    HashSet

---
+++ C# Advanced/HashSet SortedSet/05recordUniqueNames/05recordUniqueNames/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05recordUniqueNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var name = new HashSet<string>();
+            for (int i = 0; i < n; i++)
+            {
+                name.Add(Console.ReadLine());
+            }
+            Console.WriteLine(string.Join("\n ", name));
+        }
+    }
+}
