c06b39c7c0b16e105edce0e68aa0e98f1e8a2146
d77commerce <73217857+d77commerce@users.noreply.github.com>
2021-09-25 19:35:13 +0100

    new

---
+++ C# Advanced/HashSet SortedSet/05recordUniqueNames/06parkingLot/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _06parkingLot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            var carpark = new HashSet<string>();
+            while (input != "END")
+            {
+                var command = Regex.Split(input, ", ");
+                if (command[0] == "IN")
+                {
+                    carpark.Add(command[1]);
+                }
+                else
+                {
+                    carpark.Remove(command[1]);
+                }
+                input = Console.ReadLine();
+            }
+            if (carpark.Count < 1)
+            {
+                Console.WriteLine("Parking Lot is Empty");
+
+            }
+            else
+            {
+Console.WriteLine(string.Join("\n",carpark));
+            }
+            
+        }
+    }
+}
---
+++ C# Advanced/HashSet SortedSet/05recordUniqueNames/07party/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _07party
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            var taypeGuest = new Dictionary<string, Dictionary<string, HashSet<int>>>();
+            taypeGuest.Add("vip", new Dictionary<string, HashSet<int>>());
+            taypeGuest.Add("regular",new Dictionary<string, HashSet<int>>());
+            while (command != "PARTY")
+            {
+                if (Regex.IsMatch(command, @"^[\d]*"))
+                {
+                    
+                    taypeGuest["vip"].Add(command, new HashSet<int>(1));
+                    
+                }
+                else
+                {
+                    taypeGuest["regular"].Add(command, new HashSet<int>(1));
+                }
+
+                command = Console.ReadLine();
+            }
+            while (command == "END")
+            {
+
+                Console.WriteLine(string.Join("\n",taypeGuest.Values));
+            }
+        }
+    }
+}
