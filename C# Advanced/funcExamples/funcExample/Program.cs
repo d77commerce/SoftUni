4919137dabc61c1563484bc1246adcb182ef7c7e
d77commerce <73217857+d77commerce@users.noreply.github.com>
2021-09-29 23:51:23 +0100

    new

--- C# Advanced/funcExamples/funcExample/Program.cs
+++ C# Advanced/funcExamples/funcExample/Program.cs
@@ -6,7 +6,28 @@ namespace funcExample
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Func<int, int, long> func = Sum;
+            PrintResult(2, 3, func);
+        }
+        static long Multiply(int a, int b)
+        {
+            return a * b;
+        }
+        static long Sum(int a, int b)
+        {
+            return a + b;
+        }
+        static void PrintResult(int a, int b, Func<int, int, long> func)
+        {
+            for (int i = 1; i < 5; i++)
+            {
+                var result = func(a + i, b + i);
+                Console.WriteLine(new string('=', 50));
+                Console.WriteLine($" Result: {result}");
+                Console.WriteLine(new string('=', 50));
+            }
+
         }
     }
 }
