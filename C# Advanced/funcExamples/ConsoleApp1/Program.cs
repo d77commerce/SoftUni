cb202ac387bdaed819a8666bdb771c8ae8fad0e6
d77commerce <73217857+d77commerce@users.noreply.github.com>
2021-09-29 23:14:26 +0100

    Func

---
+++ C# Advanced/funcExamples/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, int, long> func = Sum;
+
+            Console.WriteLine(func(3,4));
+            Console.WriteLine(func(4,4));
+            Console.WriteLine(func(5,4));
+            Console.WriteLine(func(6,4));
+        }
+        static long Multi(int a, int b)
+        {
+            return a * b;
+        }
+        static long Sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ C# Advanced/funcExamples/linqExamples/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace linqExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long result = Multiply(2, 3);
+            Console.WriteLine($"result {result}");
+            Func<int, int, long> func = Multiply;
+            Console.WriteLine($"func1 - {func(2, 3)}");
+            Func<int, int, long> func2 = Sum;
+            Console.WriteLine($"func2 - {func2(2, 3)}");
+            Func<string> func3 = Greating;
+            
+            Console.WriteLine(func3());
+
+        }
+        static long Multiply(int a, int b)
+        {
+            return a * b;
+        }
+        static long Sum(int a, int b)
+        {
+            return a + b;
+        }
+        static string Greating()
+        {
+            return "Hallo";
+        }
+
+    }
+}
