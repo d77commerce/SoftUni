5eafd6feca8d3d35867f82b7bf8220a9187260b5
d77commerce <73217857+d77commerce@users.noreply.github.com>
2021-09-30 07:49:27 +0100

    LINQ

---
+++ C# Advanced/LINQ/linqTest/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace linqTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var listOfInt = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            var newList = listOfInt.Where(evenNum);
+            Console.WriteLine(string.Join(" ", newList));
+        }
+        static bool evenNum (int f)
+        {
+            return f % 2 == 0;
+        }
+
+    }
+}
