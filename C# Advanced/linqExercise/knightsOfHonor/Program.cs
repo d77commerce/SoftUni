e979d1e8ae68995582cf70072389b6dd6dd7eeb9
d77commerce <73217857+d77commerce@users.noreply.github.com>
2021-10-02 09:15:40 +0100

    new

---
+++ C# Advanced/linqExercise/knightsOfHonor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace knightsOfHonor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
