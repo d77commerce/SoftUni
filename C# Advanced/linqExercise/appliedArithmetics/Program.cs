59b30d2436f6117c6cf57758233c29f7b38f244c
d77commerce <73217857+d77commerce@users.noreply.github.com>
2021-10-05 12:44:56 +0100

    new

---
+++ C# Advanced/linqExercise/appliedArithmetics/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Concurrent;
+
+namespace appliedArithmetics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            Func<int[], int[]> add = num =>
+            {
+                for (int i = 0; i < num.Length; i++)
+
+                {
+                    num[i]++;
+                }
+                return num;
+            };
+            Func<int[], int[]> multiply = num =>
+            {
+                for (int i = 0; i < num.Length; i++)
+
+                {
+                    num[i] *= 2;
+                }
+                return num;
+            };
+            Func<int[], int[]> subtract = num =>
+            {
+                for (int i = 0; i < num.Length; i++)
+                {
+                    num[i]--;
+                }
+                return num;
+            };
+            while (true)
+            {
+                string comand = Console.ReadLine();
+                if (comand == "end")
+                {
+                    break;
+                }
+                else if (comand == "add")
+                {
+                    add(input);
+                }
+                else if (comand == "multiply")
+                {
+                    multiply(input);
+                }
+                else if (comand == "subtract")
+                {
+                    subtract(input);
+                }
+                else if (comand == "print")
+                {
+                    Console.WriteLine(string.Join(" ", input));
+                }
+
+            }
+        }
+    }
+}
---
+++ C# Advanced/linqExercise/reverseAndExclude/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace reverseAndExclude
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            int powerNum = int.Parse(Console.ReadLine());
+            caculate(input, powerNum);
+        }
+        static void caculate(int[] numbers, int powernum)
+        {
+            var result = new List<int>();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % powernum != 0)
+                {
+                    result.Add(numbers[i]);
+                }
+            }
+            Console.WriteLine(string.Join(" ", result).Reverse().ToArray());
+        }
+    }
+}
