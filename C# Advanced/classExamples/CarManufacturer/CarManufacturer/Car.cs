8806cf0fc31e70a30b27a9c5315cc95eaf9246b6
d77commerce <73217857+d77commerce@users.noreply.github.com>
2021-10-08 00:24:33 +0100

    new

--- C# Advanced/classExamples/CarManufacturer/CarManufacturer/Car.cs
+++ C# Advanced/classExamples/CarManufacturer/CarManufacturer/Car.cs
@@ -6,12 +6,26 @@ namespace CarManufacturer
 {
    public class Car
     {
-        private string make;
-        private string model;
-        private int year;
-        public string Make ( get; set; )
+        public string Make { get; set; }
         public string Model { get; set; }
         public int Year { get; set; }
-
+        public double FuelQuantity { get; set; }
+        public double FuelConsumation { get; set; }
+        public void Drive (double dictanse)
+        {
+            var dictanPerFuel = dictanse * (FuelConsumation / 100);
+            if (FuelQuantity - dictanPerFuel>=0)
+            {
+                FuelQuantity -= dictanPerFuel;
+            }
+            else
+            {
+                Console.WriteLine($"Not enough fuel to perform this trip!&quot");
+            }
+        }
+        public string WhoAmI()
+        {
+            return $"Make: {this.Make} Model: { this.Model} Year: { this.Year} Fuel: { this.FuelQuantity:F2}";
+        }
     }
 }
--- C# Advanced/classExamples/CarManufacturer/CarManufacturer/startUp.cs
+++ C# Advanced/classExamples/CarManufacturer/CarManufacturer/startUp.cs
@@ -2,16 +2,20 @@
 
 namespace CarManufacturer
 {
-   
     public class StartUp
     {
-       public static void Main(string[] args)
+        static void Main(string[] args)
         {
+
             Car car = new Car();
             car.Make = "BMW";
             car.Model = "320";
             car.Year = 1999;
-            Console.WriteLine($"Make: {car.Make}\nModel: {car.Model}\nYear: {car.Year}");
+            car.FuelQuantity = 150;
+            car.FuelConsumation = 15;
+            car.Drive(199);
+            Console.WriteLine(car.WhoAmI());
+
         }
     }
 }
