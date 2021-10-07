82d07cdef1037b0807e55526d06cf52226ad3638
d77commerce <73217857+d77commerce@users.noreply.github.com>
2021-10-07 21:14:00 +0100

    new

---
+++ C# Advanced/classExamples/CarManufacturer/CarManufacturer/startUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+   
+    public class StartUp
+    {
+       public static void Main(string[] args)
+        {
+            Car car = new Car();
+            car.Make = "BMW";
+            car.Model = "320";
+            car.Year = 1999;
+            Console.WriteLine($"Make: {car.Make}\nModel: {car.Model}\nYear: {car.Year}");
+        }
+    }
+}
