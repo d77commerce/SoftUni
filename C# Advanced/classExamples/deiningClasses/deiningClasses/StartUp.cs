aa84e1eed052207629fed1b6a58adab9b0406727
d77commerce <73217857+d77commerce@users.noreply.github.com>
2021-10-11 14:47:25 +0100

    New

---
+++ C# Advanced/classExamples/deiningClasses/deiningClasses/StartUp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace DefiningClasses
+{
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Family family = new Family();
+
+
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split(" " , StringSplitOptions.RemoveEmptyEntries);
+                string name = input[0];
+                int age = int.Parse(input[1]);
+                Person person = new Person(name, age);
+                family.AddMember(person);
+            }
+
+
+            foreach (var item in family.People.OrderBy(x=>x.Name))
+            {
+                if (item.Age >= 30)
+                {
+                    Console.WriteLine(item.Name + " - " + item.Age);
+                }
+
+            }
+        }
+    }
+}
