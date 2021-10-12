3ad8d2f42e57ad101d0ff542c8ccbc22fdf5db92
d77commerce <73217857+d77commerce@users.noreply.github.com>
2021-10-12 11:14:06 +0100

    New

---
+++ C# Advanced/classExamples/definingClassesExercise/definingClassesExercise/DateModifier.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace definingClassesExercise
+{
+    class DateModifier
+    {
+        public static double GetData(DateTime dateTime1, DateTime dateTime2)
+        {
+            double result = Math.Abs((dateTime1 - dateTime2).TotalDays);
+            return result;
+        }
+    }
+}
---
+++ C# Advanced/classExamples/definingClassesExercise/definingClassesExercise/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace definingClassesExercise
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] dateOne = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            DateTime date1 = new DateTime(int.Parse(dateOne[0]),
+                int.Parse(dateOne[1].Trim('0')),
+                int.Parse(dateOne[2].Trim('0')));
+            string[] dateTwo = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            DateTime date2 = new DateTime(int.Parse(dateTwo[0]), int.Parse(dateTwo[1].Trim('0')),
+                int.Parse(dateTwo[2].Trim('0')));
+            Console.WriteLine(DateModifier.GetData(date1, date2));
+            
+        }
+    }
+}
